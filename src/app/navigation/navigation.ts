e08a579da60d5be32fb21573dc6562fc9b284a54
Md. Sajib Hosen <0sajib0@gmail.com>
2019-07-18 00:20:18 +0600

    first commit

---
+++ src/app/navigation/navigation.ts
@@ -0,0 +1,32 @@
+import { FuseNavigation } from '@fuse/types';
+
+export const navigation: FuseNavigation[] = [
+    {
+        id       : 'applications',
+        title    : 'Applications',
+        translate: 'NAV.APPLICATIONS',
+        type     : 'group',
+        children : [
+            {
+                id       : 'sample',
+                title    : 'Sample',
+                translate: 'NAV.SAMPLE.TITLE',
+                type     : 'item',
+                icon     : 'email',
+                url      : '/sample',
+                badge    : {
+                    title    : '25',
+                    translate: 'NAV.SAMPLE.BADGE',
+                    bg       : '#F44336',
+                    fg       : '#FFFFFF'
+                }
+            }
+        ]
+    },
+    {
+        id   : 'register-v2',
+        title: 'Register',
+        type : 'item',
+        url  : '/pages/auth/register'
+    }
+];
